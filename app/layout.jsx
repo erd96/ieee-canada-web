2b2de523a92d172e6b54cb04d861fd6a708bb9ac
syndesa <eric.rd@live.com>
2023-09-19 16:56:07 -0400

    Create Next.js application skeleton for the IEEE Canada web competition 2023

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css'
+
+export const metadata = {
+    title: "IEEE Canada",
+    description: "IEEE Canada provides a high quality general interest magazine, a technical journal, an electronic newsletter, an annual conference, and web services designed for all IEEE Canada members, as well as support for our volunteer network of local Sections, Chapters, and Student Branches."
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+    <body>
+        <div className="main">
+
+        </div>
+        <main className="app">
+            {children}
+        </main>
+    </body>
+    </html>
+  )
+}
+
+export default RootLayout
